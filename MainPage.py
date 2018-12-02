85a09561ee896fa5773f8f6b3bd0bc09c655f553
Xiaohong Zhu <xzhu0706@gmail.com>
2018-12-01 23:54:34 -0500

    created csv files

--- MainPage.py
+++ MainPage.py
@@ -5,8 +5,13 @@
 #############################################################################
 import tkinter as tk
 from tkinter import *
+import pandas as pd
 
 class MainPage(tk.Frame):
+
+    userid = ''
+    username = ''
+    usertype = ''
 
     #Initializer
     def __init__(self, parent, controller):
@@ -16,37 +21,68 @@ class MainPage(tk.Frame):
         #This function gets called when "log in" button is clicked.
         #It checkes username and password in the file and takes the user
         #to the corresponding page depening on their user type.
-        def check_user(name, password): 
-            
-            #Opens "user_info" which stores all the username and password info
-            user_info_file = open("user_info", "r") #"r": read only
-            
-            #The while loop reads users' info in turn and matches usernames and passwords
-            while True:
-                user_id_string = user_info_file.readline()      #read user id as a string
-                
-                #Stop reading the file when there's no more information
-                if user_id_string == '':
-                    break
-                
-                user_id = int(user_id_string)                   #convert string into int
-                user_name = user_info_file.readline()           #read user_name
-                user_name = user_name.replace("\n", "")         #get rid of "\n" in user_name
-                user_password = user_info_file.readline()       #read user_password
-                user_password = user_password.replace("\n", "") #get rid of "\n" in user_password
-                user_type = user_info_file.readline()           #read user_type
-                user_type = user_type.replace("\n", "")         #get rid of "\n" in user_type
-                user_info_file.readline()                       #skip *
 
-                #If both username and password match, go to corresponding page
-                if (name == user_name and password == user_password):
-                    frame = controller.page_array[user_type]
+        def check_user(username_input, password_input):
+            accounts_db = pd.read_csv("database/UserInfos.csv", delimiter=',')
+            accounts_applications_db = pd.read_csv("database/PendingApplications.csv", delimiter=',')
+            # print(accounts_db) ## uncomment this line to see what is in db
+
+            # Check if all fields are filled
+            if username_input == '' or password_input == '':
+                tk.messagebox.showerror("Error", "Please fill out all the fields!")
+                return
+
+            # Check if username is in pending applications db
+            if not accounts_applications_db.loc[accounts_applications_db['username'] == username_input].empty:
+                tk.messagebox.showerror("Error", "Your application is still pending!")
+                return
+
+            # Check if username exists in db, if yes then check password
+            user_info = accounts_db.loc[accounts_db['username'] == username_input]
+            if user_info.empty:
+                tk.messagebox.showerror("Error", "Wrong username!")
+            else:
+                if password_input == user_info.get('password').values[0]:
+                    self.username = username_input
+                    self.userid = user_info.get('id').values[0]
+                    self.usertype = user_info.get('usertype').values[0]
+                    frame = controller.page_array[self.usertype]
                     frame.tkraise()
-                    return
-            
-            #If no match found, show the error message
-            tk.messagebox.showinfo("Login error","The username or password "\
-                                        "you entered is incorrect")
+                else:
+                    tk.messagebox.showerror("Error", "Wrong password!")
+
+
+        # def check_user(name, password):
+        #
+        #     #Opens "user_info" which stores all the username and password info
+        #     user_info_file = open("user_info", "r") #"r": read only
+        #
+        #     #The while loop reads users' info in turn and matches usernames and passwords
+        #     while True:
+        #         user_id_string = user_info_file.readline()      #read user id as a string
+        #
+        #         #Stop reading the file when there's no more information
+        #         if user_id_string == '':
+        #             break
+        #
+        #         user_id = int(user_id_string)                   #convert string into int
+        #         user_name = user_info_file.readline()           #read user_name
+        #         user_name = user_name.replace("\n", "")         #get rid of "\n" in user_name
+        #         user_password = user_info_file.readline()       #read user_password
+        #         user_password = user_password.replace("\n", "") #get rid of "\n" in user_password
+        #         user_type = user_info_file.readline()           #read user_type
+        #         user_type = user_type.replace("\n", "")         #get rid of "\n" in user_type
+        #         user_info_file.readline()                       #skip *
+        #
+        #         #If both username and password match, go to corresponding page
+        #         if (name == user_name and password == user_password):
+        #             frame = controller.page_array[user_type]
+        #             frame.tkraise()
+        #             return
+        #
+        #     #If no match found, show the error message
+        #     tk.messagebox.showinfo("Login error","The username or password "\
+        #                                 "you entered is incorrect")
 
         #############
         #    GUI    #
@@ -65,7 +101,8 @@ class MainPage(tk.Frame):
         
         #BUTTONS
         button_signup = tk.Button(self, text="Sign Up",command=lambda: controller.show_frame("SignupPage"))
-        button_login = tk.Button(self, text="Log In",command=lambda: check_user(entry_name.get(),entry_password.get()))
+        button_login = tk.Button(self, text="Log In",command=lambda: check_user(entry_name.get(),
+                                                                                entry_password.get()))
         button_guest = tk.Button(self, text="Guest User",command=lambda: controller.show_frame("Guest"))
         #**********************
         #Need to work on this
--- SignupPage.py
+++ SignupPage.py
@@ -1,48 +1,86 @@
+import pandas as pd
 import tkinter as tk
 from tkinter import *
 
 class SignupPage(tk.Frame):
     
     def __init__(self, parent, controller):
-        
-        def sign_up():
+
+        def sign_up(user_name, user_password, tech_interest):
             '''This is the function called when "sign up" is clicked'''
-            sign_up_file = open("user_info", "r+") #"r+": read and write from the begining
 
-            #the while loop reads and updates the users' info until it's the last user
-            while True:
-                user_id_string = sign_up_file.readline()
-                if user_id_string == '':
-                    break
-                user_id = int(user_id_string)
-                sign_up_file.readline() #skip user_name
-                sign_up_file.readline() #skip user_password
-                sign_up_file.readline() #skip user_type
-                sign_up_file.readline() #skip *
+            # get exisiting usernames from db to prevent duplicates
+            applications_db = pd.read_csv("database/PendingApplications.csv", delimiter=',')
+            accounts_db = pd.read_csv("database/UserInfos.csv", delimiter=',')
+            existing_pending_usernames = applications_db['username'].values.tolist()
+            existing_usernames = accounts_db['username'].values.tolist()
 
-            #get the info for the new user
-            user_id = user_id + 1         #assign a new id to the user
-            user_name = entry_name.get()      #get the user_name
-            user_password = entry_password.get()  #get the user_password
-            user_type = "OrdinaryUser"              #get the user_type
+            # Check if all fields are filled
+            if user_name == '' or user_password == '' or tech_interest == '':
+                tk.messagebox.showerror("", "Please fill out all the fields!")
+                return
 
-            #writes all the info into the file
-            print(user_id, file=sign_up_file)
-            print(user_name, file=sign_up_file)
-            print(user_password, file=sign_up_file)
-            print(user_type, file=sign_up_file)
-            print('*', file=sign_up_file)
-            sign_up_file.close()
+            # Check if username already existed in pending applications
+            if existing_pending_usernames:
+                for username in existing_pending_usernames:
+                    if user_name == username:
+                        tk.messagebox.showerror("Error", "You application is still pending, please wait for approval!")
+                        return
 
-            #clear the entries
-            entry_name.delete(0, END)
-            entry_email.delete(0, END)
-            entry_phone.delete(0, END)
-            entry_password.delete(0, END)
-            
-            tk.messagebox.showinfo("Information","Account Created, Please Login!!") #show messagebox after signup
-            controller.show_frame("MainPage")   #go back to home page
-            #end of sign_up()
+            # Check if username already existed in accounts db
+            if existing_usernames:
+                for username in existing_usernames:
+                    if user_name == username:
+                        tk.messagebox.showerror("Error", "User name already existed, please re-enter!")
+                        return
+
+            # Add account application to db
+            data = [[user_name, user_password, tech_interest]]
+            df = pd.DataFrame(data, columns=['username', 'password', 'technical_interest'])
+            with open('database/PendingApplications.csv', 'a') as pending_applications_db:
+                df.to_csv(pending_applications_db, index=False, header=False)
+            tk.messagebox.showinfo("Information", "Registration is successful, please wait for approval.")
+            controller.show_frame("MainPage")
+
+        
+        # def sign_up():
+        #     '''This is the function called when "sign up" is clicked'''
+        #     sign_up_file = open("user_info", "r+") #"r+": read and write from the begining
+        #
+        #     #the while loop reads and updates the users' info until it's the last user
+        #     while True:
+        #         user_id_string = sign_up_file.readline()
+        #         if user_id_string == '':
+        #             break
+        #         user_id = int(user_id_string)
+        #         sign_up_file.readline() #skip user_name
+        #         sign_up_file.readline() #skip user_password
+        #         sign_up_file.readline() #skip user_type
+        #         sign_up_file.readline() #skip *
+        #
+        #     #get the info for the new user
+        #     user_id = user_id + 1         #assign a new id to the user
+        #     user_name = entry_name.get()      #get the user_name
+        #     user_password = entry_password.get()  #get the user_password
+        #     user_type = "OrdinaryUser"              #get the user_type
+        #
+        #     #writes all the info into the file
+        #     print(user_id, file=sign_up_file)
+        #     print(user_name, file=sign_up_file)
+        #     print(user_password, file=sign_up_file)
+        #     print(user_type, file=sign_up_file)
+        #     print('*', file=sign_up_file)
+        #     sign_up_file.close()
+        #
+        #     #clear the entries
+        #     entry_name.delete(0, END)
+        #     entry_email.delete(0, END)
+        #     entry_phone.delete(0, END)
+        #     entry_password.delete(0, END)
+        #
+        #     tk.messagebox.showinfo("Information","Account Created, Please Login!!") #show messagebox after signup
+        #     controller.show_frame("MainPage")   #go back to home page
+        #     #end of sign_up()
 
         
         tk.Frame.__init__(self, parent)
@@ -56,17 +94,21 @@ class SignupPage(tk.Frame):
         label_email = tk.Label(self, text="Email")
         label_phone = tk.Label(self, text="Phone Number")
         label_password = tk.Label(self, text="Password")
+        label_tech_interest = tk.Label(self, text="Technical Interest")
         
         #ENTRIES
         entry_name = Entry(self)
         entry_email = Entry(self)
         entry_phone = Entry(self)
         entry_password = Entry(self,show='*')
+        entry_tech_interest = Entry(self)
 
         #BUTTONS
         button_back = tk.Button(self, text="Back",command=lambda:controller.show_frame("MainPage"))
-        button_signup = tk.Button(self, text="Sign UP",
-                            command=sign_up)
+        button_signup = tk.Button(self, text="Sign Up",
+                            command=lambda: sign_up(entry_name.get(),
+                                                    entry_password.get(),
+                                                    entry_tech_interest.get()))
     
         n = 150
         m = 25
@@ -78,16 +120,18 @@ class SignupPage(tk.Frame):
         label_email.place(x=n, y=m*7)
         label_phone.place(x=n, y=m*9)
         label_password.place(x=n, y=m*11)
+        label_tech_interest.place(x=n, y=m*13)
         
         ##PLACING THE ENTRIES
         entry_name.place(x=n+125, y=m*5)
         entry_email.place(x=n+125, y=m*7)
         entry_phone.place(x=n+125, y=m*9)
         entry_password.place(x=n+125, y=m*11)
+        entry_tech_interest.place(x=n+125, y=m*13)
         
         ##PLACING THE BUTTONS
-        button_back.place(x=150,y=350)
-        button_signup.place(x=250,y=350)
+        button_back.place(x=250,y=380)
+        button_signup.place(x=350,y=380)
 
         
         
